e8f8b1c882e3259b4ae6d0ed98e6ee50d7f75c23
Carlos Asencio Morales <carlosgkt@gmail.com>
2024-06-02 22:16:43 -0500

    adding home_routes and the dashboard,login and signup handlebars

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
+// Home page
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Login page
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+// Signup page
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+module.exports = router;
